a7345e24dd8949d442558fe9b9a7582c9e27e105
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-12 14:17:57 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
-/**main -prints "nd that piece of art is useful" - Dora Korpar, 2015-10-19",
+/**
+ * main -prints "nd that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline, to standard error.
  * Return: Always 1.
  */
