df60319028e5d56a5215edcf234edd64dffd7b28
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-12 13:31:12 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - Prints the size of various types based on 
+ * main - Prints the size of various types based on
  * the computer it is compiled and run on..
  * Return: Always 0
  */
@@ -17,5 +17,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float %zu byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
