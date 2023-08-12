ed97fdf635b67feaf65b6266867b9ab1512b9f96
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-11 17:55:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
- */ 
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle"");
