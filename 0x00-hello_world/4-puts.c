b96c93d4a4532d90f4018c20b76c9a8a3c737229
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-12 10:49:15 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
@@ -12,4 +12,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
