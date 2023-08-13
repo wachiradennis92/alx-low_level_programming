939b94694174d7b21613b3a51d5a8fa2d5a6b231
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-13 13:17:24 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'a';
+	while
+		(c <= '2') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= '2') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
