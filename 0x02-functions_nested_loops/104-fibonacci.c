309ae7921e7076c85804dbe57c87916cc523b7a9
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-15 19:14:42 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with 
+ * main - Prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, separated by a comma followed by a space
  * Return: Always 0
  */
