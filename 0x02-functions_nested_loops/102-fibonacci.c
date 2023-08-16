330d5d34ea4e983f561c5e1381a244a39739ebd0
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-15 18:24:37 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 {
 
 	int i = 0;
-	long j = 1, k =2;
+	long j = 1, k = 2;
 
 	while (i < 50)
 	{
@@ -21,7 +21,7 @@ int main(void)
 	else
 	{
 	k += j;
-	j = k -j;
+	j = k - j;
 	printf(", %ld", k);
 	}
 	++i;
