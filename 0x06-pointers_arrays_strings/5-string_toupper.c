415a992ed453619e3367803f1c7dab8d69076762
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-24 15:55:01 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,9 @@
 
 /*
  * string_toupper - Changes all lowercase letters
- * of a string to be uppercase.
+ * of a string to  uppercase.
  * @str: The string to be changed.
- * Return: A pointer to be changed string.
+ * Return: A pointer to the changed string.
  *
  */
 
