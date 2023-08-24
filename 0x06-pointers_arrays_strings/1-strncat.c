bde703fc76a78097e9bd6cc446a6a7c09701252a
wachiradennis92 <wachiradennis92@gmail.com>
2023-08-24 14:13:48 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,6 @@
 
 /**
  * _strncat - concatenates two strings
- * 
  * an inputted number of bytes from src.
  *
  * @dest: The string to be appended upon.
@@ -19,8 +18,8 @@ char *_strncat(char *dest, char *src, int n)
 
 {
 	int index = 0, dest_len = 0;
-	
-	while (dest[index++]);
+
+	while (dest[index++])
 	dest_len++;
 	for (index = 0; src[index] && index < n; index++)
 	dest[dest_len++] = src[index];
