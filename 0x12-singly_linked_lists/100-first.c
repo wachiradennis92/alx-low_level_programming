a84843a6209c8de772ed5833905c3d5b53ea5089
wachiradennis92 <wachiradennis92@gmail.com>
2023-09-21 18:25:00 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+  * first - fao
+  *
+  *
+  *
+  * Return - blank
+  */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
