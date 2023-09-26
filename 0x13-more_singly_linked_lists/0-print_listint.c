2bb5d4f64fd42e01aa74e734f29d2016ee5d20f3
wachiradennis92 <wachiradennis92@gmail.com>
2023-09-26 04:13:36 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	 size_t count;
+size_t count;
 
- 	if (h == NULL)
-		return (0);
-	for (count = 0; h != NULL; count++)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (count);
+if (h == NULL)
+return (0);
+for (count = 0; h != NULL; count++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (count);
 }
